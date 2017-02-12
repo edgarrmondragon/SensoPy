5306a29302a51260b0ba6881126d137c02beb969
EdgarRMmex <typingmonkey9201@gmail.com>
2017-02-11 20:45:35 -0600

    Changed setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@
 from distutils.core import setup
 import sensopy
 
-setup(name='sensopy',
-      version=sensopy.__version__,
-      description='TODO',
-      author='Edgar Ramírez',
-      author_email='typingmonkey9201@gmail.com',
-      # py_modules=['sensopy'])
-      packages=['sensopy', 'sensopy.discrim'])
+setup(
+    name = 'sensopy',
+    packages = ['sensopy', 'sensopy.discrim']
+    version = sensopy.__version__,
+    description = 'Python library for the design and analysis of sensory discrimination tests.',
+    author = 'Edgar Ramírez',
+    author_email = 'typingmonkey9201@gmail.com',
+    url = 'https://github.com/EdgarRMmex/SensoPy',
+    download_url = 'https://github.com/EdgarRMmex/SensoPy/tarball/0.0.1',
+    keywords = ['sensory', 'discrimination', 'triangle'],
+    classifiers = [],
+)
